b9988a6f405b271a6c6f338a78475a044d00dcda
yw0920 <ywsep20@gmail.com>
2019-03-30 23:02:49 +0000

    テーブル間の関連付け追加#2

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,7 @@ class Item < ApplicationRecord
   validates :name, presence: true, length: { maximum: 255}
   validates :url, presence: true, length: { maximum: 255}
   validates :image_url, presence: true, length: { maximum: 255}
+  
+  has_many :ownerships
+  has_many :users, throuth: :ownerships
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,7 @@ class User < ApplicationRecord
                     uniqueness: { case_sensitive: false }  
   
   has_secure_password
+  
+  has_many :ownerships
+  has_many :items, through: :ownerships
 end
