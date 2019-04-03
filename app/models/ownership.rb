6f31a7b27439b4ea925f686b1836cafe5931b76d
yw0920 <ywsep20@gmail.com>
2019-04-03 22:27:35 +0000

    wantのランキング機能追加#4

--- app/models/ownership.rb
+++ app/models/ownership.rb
@@ -2,7 +2,7 @@ class Ownership < ApplicationRecord
   belongs_to :user
   belongs_to :item
   
-  def sefl.ranking
+  def self.ranking
     self.group(:item_id).order('count_item_id DESC').limit(10).count(:item_id)
   end
 end
