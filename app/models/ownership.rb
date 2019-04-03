9c199994aa82a0e452df8e3db53fc81501d90863
yw0920 <ywsep20@gmail.com>
2019-04-03 21:54:05 +0000

    お気に入り数のカウントメソッドを追加#4

--- app/models/ownership.rb
+++ app/models/ownership.rb
@@ -1,4 +1,8 @@
 class Ownership < ApplicationRecord
   belongs_to :user
   belongs_to :item
+  
+  def sefl.ranking
+    self.group(:item_id).order('count_item_id DESC').limit(10).count(:item_id)
+  end
 end
